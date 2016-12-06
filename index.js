fd710c8aaa6d93010a70bcf2bef4e0d7974daf24
Kevin Reynolds <kevin.reynolds@endruance.com>
2016-12-06 09:18:23 -0500

    pushed the wrong code

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-/** ReactSymbol Function **/
-var ReactSymbol = function() {
-	this.returnText = '';
-  this.toString = function(symbol) {
-    if (typeof symbol === 'object' && symbol.$$typeof.toString() === 'Symbol(react.element)') {
-      if (symbol.props.children && symbol.props.children.length) {
-        Object.keys(symbol.props.children).forEach(function (key) {
-          const childValue = symbol.props.children[key];
-          switch (typeof childValue) {
-            case 'string':
-              this.returnText += childValue;
-              break;
-            case 'object':
-              this.toString(childValue);
-              break;
-            default:
-              console.warn('Neither object or value is in children props.', childValues)
-          }
-          return true;
-        });
-      }
-    } else if (typeof symbol === 'string') {
-      this.returnText += symbol;
-      return this.returnText;
+let returnText = '';
+const ReactSymbolToString = (symbol) => {
+  if (typeof symbol === 'object' && symbol.$$typeof.toString() === 'Symbol(react.element)') {
+    if (symbol.props.children && symbol.props.children.length) {
+      Object.keys(symbol.props.children).forEach((key) => {
+        const childValue = symbol.props.children[key];
+        switch (typeof childValue) {
+          case 'string':
+            returnText += childValue;
+            break;
+          case 'object':
+            ReactSymbolToString(childValue);
+            break;
+          default:
+            console.warn('Neither object or value is in children props.', childValues)
+        }
+        return true;
+      });
     }
-    return this.returnText;
-  };
-}
+  } else if (typeof symbol === 'string') {
+    returnText += symbol;
+    return returnText;
+  }
+  return returnText;
+};
 
-module.exports = new ReactSymbol();
+module.exports = ReactSymbolToString;
