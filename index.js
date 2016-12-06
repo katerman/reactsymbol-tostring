887c16b07411c53dfe4c738eb5acbab9450663c2
Kevin Reynolds <kevin.reynolds@endruance.com>
2016-12-06 16:35:46 -0500

    rewrite to fix concat issues for real

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
-const ReactSymbolToString = (symbol, retText) => {
-  let returnText = retText ? retText : '';
-  if (typeof symbol === 'object' && symbol.$$typeof.toString() === 'Symbol(react.element)') {
-    if (symbol.props.children && symbol.props.children.length) {
-      Object.keys(symbol.props.children).forEach((key) => {
-        const childValue = symbol.props.children[key];
-        switch (typeof childValue) {
-          case 'string':
-            returnText += childValue;
-            break;
-          case 'object':
-            ReactSymbolToString(childValue, returnText);
-            break;
-          default:
-            console.warn('Neither object or value is in children props.', childValues)
-        }
-        return true;
-      });
+/** ReactSymbolToString */
+function ReactSymbolToString(symbol){
+  let priv = {};
+  let pub = {};
+
+  pub.returnText = '';
+
+  priv.isSymbol = function(symbol) {
+    return typeof symbol === 'object' && symbol.$$typeof.toString() === 'Symbol(react.element)';
+  };
+
+  priv.hasChildren = function(symbol) {
+    return symbol.props.children && symbol.props.children.length
+  };
+
+  pub.getText = function(symbol) {
+    if (priv.isSymbol(symbol)) {
+      if (priv.hasChildren(symbol)) {
+
+        Object.keys(symbol.props.children).forEach((key) => {
+          const childValue = symbol.props.children[key];
+          switch (typeof childValue) {
+            case 'string':
+              pub.returnText += childValue;
+              break;
+            case 'object':
+              pub.getText(childValue);
+              break;
+            default:
+              console.warn('Neither object or value is in children props.', childValues)
+          }
+          return true;
+        });
+
+      }
+    } else if (typeof symbol === 'string') {
+      return symbol;
     }
-  } else if (typeof symbol === 'string') {
-    returnText += symbol;
+
+    return this.returnText;
   }
-  return returnText;
+
+  return pub.getText(symbol);
 };
 
 module.exports = ReactSymbolToString;
